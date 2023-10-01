66b93b8606048af9da02e771d17f38051b89afec
Nathan Istace <nathan.istace@gmail.com>
2023-10-01 22:43:56 +0200

    Menu

--- Assets/Scripts/LD54/Menu/MenuController.cs
+++ Assets/Scripts/LD54/Menu/MenuController.cs
@@ -1,11 +1,26 @@
+using NiUtils.Audio;
 using NiUtils.Extensions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 	private void Start() {
+		MenuUi.Setup();
+
 		MenuUi.onStartButtonClicked.AddListenerOnce(StartGame);
+		MenuUi.onQuitButtonClicked.AddListenerOnce(Quit);
+		MenuUi.onMusicVolumeChanged.AddListenerOnce(ChangeMusicVolume);
+		MenuUi.onSfxVolumeChanged.AddListenerOnce(ChangeSfxVolume);
 	}
+
+	private static void ChangeMusicVolume(float value) => AudioManager.Music.volume = value;
+
+	private static void ChangeSfxVolume(float value) {
+		AudioManager.Sfx.volume = value;
+		AudioManager.Sfx.PlayRandom("interact");
+	}
+
+	private static void Quit() => Application.Quit();
 
 	private static void StartGame() => SceneManager.LoadSceneAsync("Game");
 }
--- Assets/Scripts/LD54/Menu/MenuUi.cs
+++ Assets/Scripts/LD54/Menu/MenuUi.cs
@@ -1,3 +1,4 @@
+using NiUtils.Audio;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -6,10 +7,24 @@ public class MenuUi : MonoBehaviour {
 	private static MenuUi instance { get; set; }
 
 	[SerializeField] protected Button startButton;
+	[SerializeField] protected Slider musicVolumeSlider;
+	[SerializeField] protected Slider sfxVolumeSlider;
+	[SerializeField] protected Button quitButton;
 
 	public static UnityEvent onStartButtonClicked => instance.startButton.onClick;
+	public static UnityEvent<float> onMusicVolumeChanged => instance.musicVolumeSlider.onValueChanged;
+	public static UnityEvent<float> onSfxVolumeChanged => instance.sfxVolumeSlider.onValueChanged;
+	public static UnityEvent onQuitButtonClicked => instance.quitButton.onClick;
 
 	private void Awake() {
 		instance = this;
+	}
+
+	public static void Setup() {
+		instance.musicVolumeSlider.SetValueWithoutNotify(AudioManager.Music.volume);
+		instance.sfxVolumeSlider.SetValueWithoutNotify(AudioManager.Sfx.volume);
+#if UNITY_WEBGL || UNITY_EDITOR
+		instance.quitButton.gameObject.SetActive(false);
+#endif
 	}
 }
